57147a21566e2a323a936ca1e93fc835b8d66960
Erick Hora <ennh@cin.ufpe.br>
2019-06-06 12:31:17 -0300

    - Foi consertado erro de imagens não aparecendo devido a mudança de localização da pasta utilites; - Foi adicionado o método GET para um orçamento, que vai ser usado para visualização de um único orçamento.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const Budget = require('./models/budget');
+const Budget = require("./models/budget");
 
 const app = express();
 
 //db senha: t8loXPLkeb6aYADG
-mongoose.connect("mongodb+srv://erickhora:t8loXPLkeb6aYADG@esatt-db-macdv.mongodb.net/esatt?retryWrites=true&w=majority")
+mongoose
+  .connect(
+    "mongodb+srv://erickhora:t8loXPLkeb6aYADG@esatt-db-macdv.mongodb.net/esatt?retryWrites=true&w=majority"
+  )
   .then(() => {
-    console.log('Conexão com banco de dados feita com sucesso!');
+    console.log("Conexão com banco de dados feita com sucesso!");
   })
   .catch(() => {
-    console.log('Erro na conxeão com banco de dados');
+    console.log("Erro na conxeão com banco de dados");
   });
 
 app.use(bodyParser.json());
@@ -31,36 +34,48 @@ app.use((req, res, next) => {
   next();
 });
 
+//GET Todos os orçamentos
+app.get("/api/budgets", (req, res, next) => {
+  Budget.find().then(documents => {
+    res.status(200).json({
+      message: "Orçamentos recuperados com sucesso!",
+      budgets: documents
+    });
+  });
+});
+
+//POST Um orçamento
 app.post("/api/budgets", (req, res, next) => {
   const budget = new Budget({
     name: req.body.name,
     description: req.body.description,
     reference: req.body.reference,
     content: null,
-    creator: 'Erick'
+    creator: "Erick"
   });
   budget.save().then(createdBudget => {
     res.status(201).json({
-      message: 'Um novo orçamento foi criado com sucesso.',
+      message: "Um novo orçamento foi criado com sucesso.",
       budgetId: createdBudget._id
     });
   });
 });
 
-app.get("/api/budgets", (req, res, next) => {
-  Budget.find()
-    .then(documents => {
-      res.status(200).json({
-        message: "Posts fetched succesfully!",
-        budgets: documents
-      });
+//GET Um orçamento
+app.get("/api/budgets/:id", (req, res, next) => {
+  Budget.findOne({_id: req.params.id }).then(result => {
+    res.status(200).json({
+      message: "Orçamento " + result._id + " recuperado com sucesso!", //Se der errado substituir _id por id
+      budget: result
     });
+  });
 });
 
-app.delete('/api/budgets/:id', (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
+//DELETE Um orçamento
+app.delete("/api/budgets/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
-    res.status(200).json({message: 'Orçamento deletado!'});
+    res.status(200).json({ message: "Orçamento deletado!" });
   });
 });
 
--- src/app/services/budgets.service.ts
+++ src/app/services/budgets.service.ts
@@ -5,38 +5,52 @@ import { map } from 'rxjs/operators';
 
 import { Budget } from '../models/budget.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class BudgetsService {
   private budgets: Budget[] = [];
+  private budget: Budget;
   private budgetsUpdated = new Subject<Budget[]>();
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  getBudgets(){
-    this.http.get<{message: string, budgets: any}>('http://localhost:3000/api/budgets')
-      .pipe(map((budgetData) => {
-        return budgetData.budgets.map((budget) => {
-          return {
-            name: budget.name,
-            description: budget.description,
-            reference: budget.reference,
-            content: budget.content,
-            creator: budget.creator,
-            id: budget._id
-          }
-        });
-      }))
-      .subscribe((transformedBudget) => {
+  // GET Todos os orçamentos
+  getBudgets() {
+    this.http
+      .get<{ message: string; budgets: any }>(
+        'http://localhost:3000/api/budgets'
+      )
+      .pipe(
+        map(budgetData => {
+          return budgetData.budgets.map(budget => {
+            return {
+              name: budget.name,
+              description: budget.description,
+              reference: budget.reference,
+              content: budget.content,
+              creator: budget.creator,
+              id: budget._id
+            };
+          });
+        })
+      )
+      .subscribe(transformedBudget => {
         this.budgets = transformedBudget;
         this.budgetsUpdated.next([...this.budgets]);
       });
   }
 
-  getBudgetUpdateListener(){
+  getBudgetUpdateListener() {
     return this.budgetsUpdated.asObservable();
   }
 
-  postBudget(name: string, description: string, reference: string, content: object, creator: string){
+  // POST Um orçamento
+  postBudget(
+    name: string,
+    description: string,
+    reference: string,
+    content: object,
+    creator: string
+  ) {
     const budget: Budget = {
       id: null,
       name,
@@ -44,9 +58,13 @@ export class BudgetsService {
       reference,
       content,
       creator
-    }
-    this.http.post<{message: string, budgetId: string}>('http://localhost:3000/api/budgets', budget)
-      .subscribe((responseData) => {
+    };
+    this.http
+      .post<{ message: string; budgetId: string }>(
+        'http://localhost:3000/api/budgets',
+        budget
+      )
+      .subscribe(responseData => {
         const id = responseData.budgetId;
         budget.id = id;
         this.budgets.push(budget);
@@ -54,10 +72,25 @@ export class BudgetsService {
       });
   }
 
-  deleteBudget(budgetId: string){
-    this.http.delete('http://localhost:3000/api/budgets/' + budgetId)
+  // GET Um orçamento
+  getBudget(budgetId: string) {
+    this.http
+      .get<{ message: string; budget: Budget }>(
+        'http://localhost:3000/api/budgets/' + budgetId
+      )
+      .subscribe(budgetData => {
+        this.budget = budgetData.budget;
+      });
+  }
+
+  // DELETE Um orçamento
+  deleteBudget(budgetId: string) {
+    this.http
+      .delete('http://localhost:3000/api/budgets/' + budgetId)
       .subscribe(() => {
-        const updatedBudgets = this.budgets.filter(budget => budget.id !== budgetId);
+        const updatedBudgets = this.budgets.filter(
+          budget => budget.id !== budgetId
+        );
         this.budgets = updatedBudgets;
         this.budgetsUpdated.next([...this.budgets]);
       });
