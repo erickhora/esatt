a957f7fc9eb26ffc0455f20a94c0a2b7b5082ad1
Erick Hora <ennh@cin.ufpe.br>
2019-06-15 01:16:25 -0300

    - Adicionado botão de uploado de quantitativos; - Revertido para o estado estável do método GET de um orçamento.

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { BudgetsService } from '../services/budgets.service';
 
@@ -13,24 +13,43 @@ export class CreateProjectComponent implements OnInit {
   enteredName = '';
   enteredDescription = '';
   enteredReference = '';
+  form: FormGroup;
 
   constructor(private router: Router, public budgetsService: BudgetsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      reference: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      file: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
+  }
 
-  onAddBudget(form: NgForm) {
-    if (form.invalid) {
+  onAddBudget() {
+    if (this.form.invalid) {
       return;
     }
     this.budgetsService.postBudget(
-      form.value.name,
-      form.value.description,
-      form.value.reference,
+      this.form.value.name,
+      this.form.value.description,
+      this.form.value.reference,
       null,
       ''
     );
   }
-  // onCreateRedirect(){
-  //   this.router.navigate(['home/new-project/budget']);
-  // }
+
+  onAddedQuantity(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({file});
+    this.form.get('file').updateValueAndValidity();
+  }
 }
